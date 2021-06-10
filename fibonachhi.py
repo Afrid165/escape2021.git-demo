879468fe56f5ffb0723ed05244287f30877f7d93
Afrid165 <syedafridjahan268@gmail.com>
2021-06-10 11:09:58 +0530

    Apply cache to fibonachhi.py. Much faster.

--- fibonachhi.py
+++ fibonachhi.py
@@ -1,9 +1,11 @@
+from functools import cache
+@cache
 def fibonachhi(n):
-	if n == 0:
-           return 0
-        if n == 1:
-           return 1
-        return fibonachhi(n-1) + fibonachhi(n-2)
+ if n == 0:
+   return 0
+ if n == 1:
+   return 1
+ return fibonachhi(n-1) + fibonachhi(n-2)
 
-if _name_ == '_main_':
-	print(fibonachhi(20))
+if __name__ == '__main__':
+ print(fibonachhi(30))
